c7248e541e6330b51c53eafadbbe7603288d0b4c
marco9325 <nelis.marc@gmail.com>
2011-05-14 16:07:02 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuiviFacturation::Application.config.secret_token = 'b8627ab3c8622fcdadb0205ebe7c4b31e2c5eebb34f63cf7d13f64118e6a107bd8def03c33a5632585981e82718b3118f702f31c6bc4d4e9e45b85b3ceae9e31'
